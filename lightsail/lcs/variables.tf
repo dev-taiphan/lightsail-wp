f1411db006c8dfe5b1023d5dadac5661afebda5b
zenlunaris <133255662+dev-taiphan@users.noreply.github.com>
2025-07-08 09:13:13 +0700

    Create variables.tf

---
+++ lightsail/lcs/variables.tf
@@ -0,0 +1,11 @@
+variable "lcs_service_name" {
+  description = "Name of the Lightsail Container Service"
+}
+
+variable "lcs_container_name" {
+  description = "Name of the container in Lightsail"
+}
+
+variable "env" {
+  description = "Deployment environment (dev1, prd, etc.)"
+}
